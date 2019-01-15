471c0efc7c2c2f44bb7a2e80f1a3b1d1feaf1b9f
haibing.cao <haibing.cao>
2019-01-15 15:39:17 +0800

    code optimize

--- disconf-client/src/main/java/com/baidu/disconf/client/support/utils/ClassUtils.java
+++ disconf-client/src/main/java/com/baidu/disconf/client/support/utils/ClassUtils.java
@@ -7,16 +7,11 @@ import java.util.Set;
 
 /**
  * 类工具
- *
- * @author liaoqiqi
- * @version 2014-6-11
  */
 public class ClassUtils {
 
     /**
      * 由Get Method名称获取Field名
-     *
-     * @return
      */
     public static String getFieldNameByGetMethodName(String methodName) {
 
@@ -25,20 +20,15 @@ public class ClassUtils {
         // 必须以get或is开始的
         if (methodName.startsWith("get")) {
             prefixIndex = 3;
-
         } else if (methodName.startsWith("is")) {
-
             prefixIndex = 2;
-
         } else {
-
             return null;
         }
 
         String fieldName = methodName.substring(prefixIndex);
         if (fieldName.length() >= 1) {
-            String firstCharStr = String.valueOf(fieldName.charAt(0))
-                    .toLowerCase();
+            String firstCharStr = String.valueOf(fieldName.charAt(0)).toLowerCase();
             if (fieldName.length() > 1) {
                 fieldName = firstCharStr + fieldName.substring(1);
             } else {
@@ -51,122 +41,81 @@ public class ClassUtils {
 
     /**
      * 根据Field类型设置值
-     *
-     * @param field
      */
     public static void setFieldValeByType(Field field, Object obj, String value)
             throws Exception {
 
         Class<?> type = field.getType();
-
         String typeName = type.getName();
 
         if (typeName.equals("int")) {
-
             if (value.equals("")) {
                 value = "0";
             }
             field.set(obj, Integer.valueOf(value));
-
         } else if (typeName.equals("long")) {
-
             if (value.equals("")) {
                 value = "0";
             }
             field.set(obj, Long.valueOf(value));
-
         } else if (typeName.equals("boolean")) {
-
             if (value.equals("")) {
                 value = "false";
             }
             field.set(obj, Boolean.valueOf(value));
-
         } else if (typeName.equals("double")) {
-
             if (value.equals("")) {
                 value = "0.0";
             }
             field.set(obj, Double.valueOf(value));
-
         } else {
-
             field.set(obj, value);
         }
     }
 
     /**
      * 根据Field类型返回值
-     *
-     * @param type
-     * @param value
-     *
-     * @return
-     *
-     * @throws Exception
      */
-    public static Object getValeByType(Class<?> type, Object value)
-            throws Exception {
+    public static Object getValeByType(Class<?> type, Object value) throws Exception {
 
         // 预处理
         if (!(value instanceof String)) {
             value = "";
         }
 
-        // trim
         String dataValue = (String) value;
         dataValue = dataValue.trim();
 
         // process
-        String typeName = type.getName();
-        typeName = typeName.toLowerCase();
-
+        String typeName = type.getName().toLowerCase();
         if (typeName.equals("int") || typeName.equals("java.lang.integer")) {
-
             if (value.equals("")) {
                 value = "0";
             }
-
             return Integer.valueOf(dataValue);
-
         } else if (typeName.equals("long") || typeName.equals("java.lang.long")) {
-
             if (value.equals("")) {
                 value = "0";
             }
-
             return Long.valueOf(dataValue);
-
-        } else if (typeName.equals("boolean")
-                || typeName.equals("java.lang.boolean")) {
-
+        } else if (typeName.equals("boolean") || typeName.equals("java.lang.boolean")) {
             if (value.equals("")) {
                 value = "false";
             }
-
             return Boolean.valueOf(dataValue);
 
-        } else if (typeName.equals("double")
-                || typeName.equals("java.lang.double")) {
-
+        } else if (typeName.equals("double") || typeName.equals("java.lang.double")) {
             if (value.equals("")) {
                 value = "0.0";
             }
-
             return Double.valueOf(dataValue);
-
         } else {
-
             return value;
         }
     }
 
     /**
      * 获取一个类的所有方法
-     *
-     * @param entityClass
-     *
-     * @return
      */
     public static Set<Method> getAllMethod(Class<?> entityClass) {
 
