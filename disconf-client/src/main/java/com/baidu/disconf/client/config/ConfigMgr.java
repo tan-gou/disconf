51f94a5bfe7d20f5ba30fe62106f22320c4c69c7
haibing.cao <haibing.cao>
2019-01-15 16:52:48 +0800

    code optimize

--- disconf-client/src/main/java/com/baidu/disconf/client/config/ConfigMgr.java
+++ disconf-client/src/main/java/com/baidu/disconf/client/config/ConfigMgr.java
@@ -8,9 +8,6 @@ import com.baidu.disconf.client.config.inner.DisInnerConfigHelper;
 
 /**
  * 配置模块
- *
- * @author liaoqiqi
- * @version 2014-6-6
  */
 public class ConfigMgr {
 
@@ -20,51 +17,26 @@ public class ConfigMgr {
 
     /**
      * 初始化配置
-     *
-     * @throws Exception
      */
     public synchronized static void init() throws Exception {
 
         LOGGER.info("--------------- LOAD CONFIG START ---------------");
-
-        //
         LOGGER.info("Finer print: " + DisClientComConfig.getInstance().getInstanceFingerprint());
 
-        // 导入系统配置
+        /** 导入 disconf 系统配置 */
         DisClientSysConfig.getInstance().loadConfig(null);
-
-        // 校验 系统配置
         DisInnerConfigHelper.verifySysConfig();
 
-        // 导入用户配置
+        /** 导入 disconf client 配置 */
         DisClientConfig.getInstance().loadConfig(null);
-
-        // 校验 用户配置
         DisInnerConfigHelper.verifyUserConfig();
 
         isInit = true;
-
         LOGGER.info("--------------- LOAD CONFIG END ---------------");
     }
 
-    /**
-     */
+
     public synchronized static boolean isInit() {
         return isInit;
     }
-
-    /**
-     */
-    public static void main(String[] args) {
-
-        try {
-
-            ConfigMgr.init();
-
-        } catch (Exception e) {
-
-            e.printStackTrace();
-        }
-    }
-
 }
