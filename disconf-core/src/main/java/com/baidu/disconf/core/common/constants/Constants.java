f22d6b55b95b427ea38b8b40b621ef6ac6e9b6da
haibing.cao <haibing.cao>
2019-01-15 16:24:50 +0800

    code optimize

--- disconf-core/src/main/java/com/baidu/disconf/core/common/constants/Constants.java
+++ disconf-core/src/main/java/com/baidu/disconf/core/common/constants/Constants.java
@@ -1,9 +1,6 @@
 package com.baidu.disconf.core.common.constants;
 
-/**
- * @author liaoqiqi
- * @version 2014-6-6
- */
+
 public class Constants {
 
     public static final String DEFAULT_VERSION = "DEFAULT_VERSION";
