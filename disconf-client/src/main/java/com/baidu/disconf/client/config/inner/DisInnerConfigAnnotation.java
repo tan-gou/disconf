fa69345dd05a3d84d76d3e2ff4c1949bcc7e648a
haibing.cao <haibing.cao>
2019-01-15 16:25:23 +0800

    code optimize

--- disconf-client/src/main/java/com/baidu/disconf/client/config/inner/DisInnerConfigAnnotation.java
+++ disconf-client/src/main/java/com/baidu/disconf/client/config/inner/DisInnerConfigAnnotation.java
@@ -8,9 +8,6 @@ import java.lang.annotation.Target;
 
 /**
  * Disconf 自己的配置文件中的项标注
- *
- * @author liaoqiqi
- * @version 2014-6-16
  */
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
