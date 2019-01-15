0b81c701b2047cc9d886ef5c562ba176d13aa164
haibing.cao <haibing.cao>
2019-01-15 11:31:29 +0800

    code optimize

--- disconf-client/src/main/java/com/baidu/disconf/client/common/model/DisconfCenterBaseModel.java
+++ disconf-client/src/main/java/com/baidu/disconf/client/common/model/DisconfCenterBaseModel.java
@@ -2,23 +2,30 @@ package com.baidu.disconf.client.common.model;
 
 /**
  * 配置基类
- *
- * @author liaoqiqi
- * @version 2014-8-4
  */
 public abstract class DisconfCenterBaseModel {
 
-    // 所在类实体
+    /**
+     * 所在类实体
+     */
     private Object object;
 
-    // 远程配置服务的URL路径,不包含IP和PORT的
+    /**
+     * 远程配置服务的URL路径,不包含IP和PORT的
+     */
     private String remoteServerUrl;
 
-    // 通用配置
+    /**
+     * 通用配置
+     */
     private DisConfCommonModel disConfCommonModel = new DisConfCommonModel();
 
-    // 回调函数
+    /**
+     * 回调函数
+     */
     private DisconfCommonCallbackModel disconfCommonCallbackModel = new DisconfCommonCallbackModel();
+
+
 
     public DisConfCommonModel getDisConfCommonModel() {
         return disConfCommonModel;
@@ -28,6 +35,7 @@ public abstract class DisconfCenterBaseModel {
         this.disConfCommonModel = disConfCommonModel;
     }
 
+
     public DisconfCommonCallbackModel getDisconfCommonCallbackModel() {
         return disconfCommonCallbackModel;
     }
@@ -35,6 +43,7 @@ public abstract class DisconfCenterBaseModel {
     public void setDisconfCommonCallbackModel(DisconfCommonCallbackModel disconfCommonCallbackModel) {
         this.disconfCommonCallbackModel = disconfCommonCallbackModel;
     }
+
 
     public String getRemoteServerUrl() {
         return remoteServerUrl;
