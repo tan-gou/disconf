d0f1c424e0e8cf70ba29a809351463fb1643d5a6
haibing.cao <haibing.cao>
2019-01-16 14:39:23 +0800

    code optimize

--- disconf-core/src/main/java/com/baidu/disconf/core/common/restful/RestfulFactory.java
+++ disconf-core/src/main/java/com/baidu/disconf/core/common/restful/RestfulFactory.java
@@ -3,23 +3,14 @@ package com.baidu.disconf.core.common.restful;
 import com.baidu.disconf.core.common.restful.impl.RestfulMgrImpl;
 import com.baidu.disconf.core.common.restful.retry.impl.RetryStrategyRoundBin;
 
-/**
- * @author liaoqiqi
- * @version 2014-8-4
- */
+
 public class RestfulFactory {
 
     /**
      * 获取一个默认的抓取器
-     *
-     * @return
-     *
-     * @throws Exception
      */
-    public static RestfulMgr getRestfulMgrNomal() throws Exception {
-
+    public static RestfulMgr getRestfulMgrNomal() {
         RestfulMgr restfulMgr = new RestfulMgrImpl(new RetryStrategyRoundBin());
-
         return restfulMgr;
     }
 }
