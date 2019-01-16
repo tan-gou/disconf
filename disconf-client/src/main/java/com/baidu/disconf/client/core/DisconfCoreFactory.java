9f79b35cc02c2c0d2cedebc6d53958778df65f87
haibing.cao <haibing.cao>
2019-01-16 20:33:28 +0800

    code optimize

--- disconf-client/src/main/java/com/baidu/disconf/client/core/DisconfCoreFactory.java
+++ disconf-client/src/main/java/com/baidu/disconf/client/core/DisconfCoreFactory.java
@@ -10,22 +10,15 @@ import com.baidu.disconf.client.watch.WatchMgr;
 
 /**
  * Core模块的工厂类
- *
- * @author liaoqiqi
- * @version 2014-7-29
  */
 public class DisconfCoreFactory {
 
-    /**
-     * @throws Exception
-     */
     public static DisconfCoreMgr getDisconfCoreMgr(Registry registry) throws Exception {
 
         FetcherMgr fetcherMgr = FetcherFactory.getFetcherMgr();
 
         //
         // 不开启disconf，则不要watch了
-        //
         WatchMgr watchMgr = null;
         if (DisClientConfig.getInstance().ENABLE_DISCONF) {
             // Watch 模块
