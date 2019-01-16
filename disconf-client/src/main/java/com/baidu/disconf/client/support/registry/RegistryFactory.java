225034aafd30e3dac0e0e647cc1104e05817fd56
haibing.cao <haibing.cao>
2019-01-16 16:04:42 +0800

    code optimize

--- disconf-client/src/main/java/com/baidu/disconf/client/support/registry/RegistryFactory.java
+++ disconf-client/src/main/java/com/baidu/disconf/client/support/registry/RegistryFactory.java
@@ -4,15 +4,10 @@ import org.springframework.context.ApplicationContext;
 
 import com.baidu.disconf.client.support.registry.impl.SpringRegistry;
 
-/**
- * Created by knightliao on 15/11/26.
- */
+
 public class RegistryFactory {
 
-    /**
-     *
-     */
-    public static Registry getSpringRegistry(ApplicationContext applicationContext) throws Exception {
+    public static Registry getSpringRegistry(ApplicationContext applicationContext) {
 
         SpringRegistry registry = new SpringRegistry();
         registry.setApplicationContext(applicationContext);
