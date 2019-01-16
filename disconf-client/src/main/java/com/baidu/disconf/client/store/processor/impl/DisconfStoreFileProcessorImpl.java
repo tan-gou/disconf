84c50b86219f8287653e08d8e8127b8151ff1575
haibing.cao <haibing.cao>
2019-01-16 10:09:48 +0800

    code optimize

--- disconf-client/src/main/java/com/baidu/disconf/client/store/processor/impl/DisconfStoreFileProcessorImpl.java
+++ disconf-client/src/main/java/com/baidu/disconf/client/store/processor/impl/DisconfStoreFileProcessorImpl.java
@@ -22,65 +22,44 @@ import com.baidu.disconf.client.store.processor.model.DisconfValue;
 
 /**
  * 配置文件仓库实现器
- *
- * @author liaoqiqi
- * @version 2014-8-4
  */
 public class DisconfStoreFileProcessorImpl implements DisconfStoreProcessor {
 
     protected static final Logger LOGGER = LoggerFactory.getLogger(DisconfStoreFileProcessorImpl.class);
 
-    /**
-     *
-     */
+
     @Override
     public void addUpdateCallbackList(String keyName, List<IDisconfUpdate> iDisconfUpdateList) {
-
         if (getInstance().getConfFileMap().containsKey(keyName)) {
-
             getInstance().getConfFileMap().get(keyName).getDisconfCommonCallbackModel().getDisconfConfUpdates()
                     .addAll(iDisconfUpdateList);
         }
     }
 
-    /**
-     *
-     */
+
     @Override
     public List<IDisconfUpdate> getUpdateCallbackList(String keyName) {
 
         if (getInstance().getConfFileMap().containsKey(keyName)) {
-
             return getInstance().getConfFileMap().get(keyName).getDisconfCommonCallbackModel().getDisconfConfUpdates();
         }
-
         return new ArrayList<IDisconfUpdate>();
     }
 
-    /**
-     *
-     */
     @Override
     public DisConfCommonModel getCommonModel(String keyName) {
 
         DisconfCenterFile disconfCenterFile = getInstance().getConfFileMap().get(keyName);
-
-        // 校验是否存在
         if (disconfCenterFile == null) {
             LOGGER.error("cannot find " + keyName + " in store....");
             return null;
         }
-
         return disconfCenterFile.getDisConfCommonModel();
     }
 
-    /**
-     *
-     */
+
     @Override
     public boolean hasThisConf(String keyName) {
-
-        // 配置文件
         if (!getInstance().getConfFileMap().containsKey(keyName)) {
             return false;
         }
@@ -88,82 +67,53 @@ public class DisconfStoreFileProcessorImpl implements DisconfStoreProcessor {
     }
 
     /**
-     *
+     * 为 类的属性 注入值
      */
     @Override
     public void inject2Instance(Object object, String fileName) {
 
         DisconfCenterFile disconfCenterFile = getInstance().getConfFileMap().get(fileName);
-
-        // 校验是否存在
         if (disconfCenterFile == null) {
             LOGGER.error("cannot find " + fileName + " in store....");
             return;
         }
 
-        //
-        // 静态类
-        //
+        // 非静态类
         if (object != null) {
-            // 设置object
             disconfCenterFile.setObject(object);
         }
 
-        // 根据类型设置值
-        //
-        // 注入实体
-        //
+        // 根据类型设置值，注入实体
         Map<String, FileItemValue> keMap = disconfCenterFile.getKeyMaps();
         for (String fileItem : keMap.keySet()) {
-
-            // 根据类型设置值
             try {
-
-                //
-                // 静态类
-                //
-                if (object == null) {
-
+                if (object == null) {   // 静态类
                     if (keMap.get(fileItem).isStatic()) {
                         LOGGER.debug(fileItem + " is a static field. ");
                         keMap.get(fileItem).setValue4StaticFileItem(keMap.get(fileItem).getValue());
                     }
-
-                    //
-                    // 非静态类
-                    //
-                } else {
-
+                } else { // 非静态类
                     LOGGER.debug(fileItem + " is a non-static field. ");
-
                     if (keMap.get(fileItem).getValue() == null) {
-
                         // 如果仓库值为空，则实例 直接使用默认值
                         Object defaultValue = keMap.get(fileItem).getFieldDefaultValue(object);
                         keMap.get(fileItem).setValue(defaultValue);
-
                     } else {
-
                         // 如果仓库里的值为非空，则实例使用仓库里的值
                         keMap.get(fileItem).setValue4FileItem(object, keMap.get(fileItem).getValue());
                     }
                 }
-
             } catch (Exception e) {
                 LOGGER.error("inject2Instance fileName " + fileName + " " + e.toString(), e);
             }
         }
     }
 
-    /**
-     *
-     */
+
     @Override
     public Object getConfig(String fileName, String keyName) {
 
         DisconfCenterFile disconfCenterFile = getInstance().getConfFileMap().get(fileName);
-
-        // 校验是否存在
         if (disconfCenterFile == null) {
             LOGGER.debug("cannot find " + fileName + " in store....");
             return null;
@@ -178,14 +128,12 @@ public class DisconfStoreFileProcessorImpl implements DisconfStoreProcessor {
     }
 
     /**
-     *
+     * 更新配置仓库的 值
      */
     @Override
     public void inject2Store(String fileName, DisconfValue disconfValue) {
 
         DisconfCenterFile disconfCenterFile = getInstance().getConfFileMap().get(fileName);
-
-        // 校验是否存在
         if (disconfCenterFile == null) {
             LOGGER.error("cannot find " + fileName + " in store....");
             return;
@@ -199,21 +147,18 @@ public class DisconfStoreFileProcessorImpl implements DisconfStoreProcessor {
         // 存储
         Map<String, FileItemValue> keMap = disconfCenterFile.getKeyMaps();
         if (keMap.size() > 0) {
-            for (String fileItem : keMap.keySet()) {
-
-                Object object = disconfValue.getFileData().get(fileItem);
+            for (String key : keMap.keySet()) {
+                Object object = disconfValue.getFileData().get(key);
                 if (object == null) {
-                    LOGGER.error("cannot find {} to be injected. file content is: {}", fileItem,
+                    LOGGER.error("cannot find {} to be injected. file content is: {}", key,
                             disconfValue.getFileData().toString());
                     continue;
                 }
 
                 // 根据类型设置值
                 try {
-
-                    Object value = keMap.get(fileItem).getFieldValueByType(object);
-                    keMap.get(fileItem).setValue(value);
-
+                    Object value = keMap.get(key).getFieldValueByType(object);
+                    keMap.get(key).setValue(value);
                 } catch (Exception e) {
                     LOGGER.error("inject2Store filename: " + fileName + " " + e.toString(), e);
                 }
@@ -222,7 +167,6 @@ public class DisconfStoreFileProcessorImpl implements DisconfStoreProcessor {
 
         // 使用过 XML式配置
         if (disconfCenterFile.isTaggedWithNonAnnotationFile()) {
-
             if (disconfCenterFile.getSupportFileTypeEnum().equals(SupportFileTypeEnum.PROPERTIES)) {
                 // 如果是采用XML进行配置的，则需要利用spring的reload将数据reload到bean里
                 ReloadConfigurationMonitor.reload();
