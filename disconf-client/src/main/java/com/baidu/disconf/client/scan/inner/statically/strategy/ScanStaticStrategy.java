a798a7e646af29eeb26ebce9eb7d42948b1d65b8
haibing.cao <haibing.cao>
2019-01-16 16:12:44 +0800

    code optimize

--- disconf-client/src/main/java/com/baidu/disconf/client/scan/inner/statically/strategy/ScanStaticStrategy.java
+++ disconf-client/src/main/java/com/baidu/disconf/client/scan/inner/statically/strategy/ScanStaticStrategy.java
@@ -6,9 +6,6 @@ import com.baidu.disconf.client.scan.inner.statically.model.ScanStaticModel;
 
 /**
  * 扫描静态注解，并且进行分析整合数据
- *
- * @author liaoqiqi
- * @version 2014-6-6
  */
 public interface ScanStaticStrategy {
 
