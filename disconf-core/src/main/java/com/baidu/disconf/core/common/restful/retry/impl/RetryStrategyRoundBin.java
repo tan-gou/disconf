dddce55c0b89171018a5601de90c2b9555a82c55
haibing.cao <haibing.cao>
2019-01-16 14:22:31 +0800

    code optimize

--- disconf-core/src/main/java/com/baidu/disconf/core/common/restful/retry/impl/RetryStrategyRoundBin.java
+++ disconf-core/src/main/java/com/baidu/disconf/core/common/restful/retry/impl/RetryStrategyRoundBin.java
@@ -8,32 +8,19 @@ import com.baidu.disconf.core.common.restful.retry.RetryStrategy;
 
 /**
  * 轮循重试
- *
- * @author liaoqiqi
- * @version 2014-8-4
  */
 public class RetryStrategyRoundBin implements RetryStrategy {
 
     protected static final Logger LOGGER = LoggerFactory.getLogger(RetryStrategyRoundBin.class);
 
-    /**
-     * @param unreliableImpl
-     * @param retryTimes
-     * @param sleepSeconds
-     */
     public <T> T retry(UnreliableInterface unreliableImpl, int retryTimes, int sleepSeconds) throws Exception {
 
         int cur_time = 0;
         for (; cur_time < retryTimes; ++cur_time) {
-
             try {
-
                 return unreliableImpl.call();
-
             } catch (Exception e) {
-
                 LOGGER.warn("cannot reach, will retry " + cur_time + " .... " + e.toString());
-
                 try {
                     Thread.sleep(sleepSeconds * 1000);
                 } catch (InterruptedException e1) {
@@ -42,7 +29,6 @@ public class RetryStrategyRoundBin implements RetryStrategy {
         }
 
         LOGGER.warn("finally failed....");
-
         throw new Exception();
     }
 }
