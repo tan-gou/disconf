a9d8116f69778d9b7f1bc20b44f97b91bf6b5822
haibing.cao <haibing.cao>
2019-01-16 11:38:25 +0800

    code optimize

--- disconf-core/src/main/java/com/baidu/disconf/core/common/restful/retry/RetryStrategy.java
+++ disconf-core/src/main/java/com/baidu/disconf/core/common/restful/retry/RetryStrategy.java
@@ -4,12 +4,8 @@ import com.baidu.disconf.core.common.restful.core.UnreliableInterface;
 
 /**
  * 重试的策略
- *
- * @author liaoqiqi
- * @version 2014-6-10
  */
 public interface RetryStrategy {
 
     <T> T retry(UnreliableInterface unreliableImpl, int retryTimes, int sleepSeconds) throws Exception;
-
 }
