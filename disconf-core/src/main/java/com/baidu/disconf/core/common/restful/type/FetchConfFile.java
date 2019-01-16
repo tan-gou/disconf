1227ba97e47f33e154738089d8d8860fbe157273
haibing.cao <haibing.cao>
2019-01-16 11:52:40 +0800

    code optimize

--- disconf-core/src/main/java/com/baidu/disconf/core/common/restful/type/FetchConfFile.java
+++ disconf-core/src/main/java/com/baidu/disconf/core/common/restful/type/FetchConfFile.java
@@ -12,24 +12,17 @@ import com.baidu.disconf.core.common.utils.OsUtil;
 
 /**
  * 下载文件
- *
- * @author liaoqiqi
  */
 public class FetchConfFile implements UnreliableInterface {
 
     protected static final Logger LOGGER = LoggerFactory.getLogger(FetchConfFile.class);
 
-    private URL remoteUrl;
+    // 远程地址
+    private URL  remoteUrl;
+    // 本地文件
     private File localTmpFile;
 
-    /**
-     * 远程文件，本地文件
-     *
-     * @param remoteUrl
-     * @param localTmpFile
-     */
     public FetchConfFile(URL remoteUrl, File localTmpFile) {
-
         this.remoteUrl = remoteUrl;
         this.localTmpFile = localTmpFile;
     }
@@ -41,26 +34,18 @@ public class FetchConfFile implements UnreliableInterface {
     public <T> T call() throws Exception {
 
         // 删除临时文件
-        // LOGGER.info("start to remove tmp download file: " + ""
-        // + localTmpFile.getAbsolutePath());
         if (localTmpFile.exists()) {
             localTmpFile.delete();
         }
-
-        // start tp download
         LOGGER.debug("start to download. From: " + remoteUrl + " , TO: " + localTmpFile.getAbsolutePath());
 
-        // 下载
         FileUtils.copyURLToFile(remoteUrl, localTmpFile);
 
-        // check
         if (!OsUtil.isFileExist(localTmpFile.getAbsolutePath())) {
             throw new Exception("download is ok, but cannot find downloaded file." + localTmpFile);
         }
 
-        // download success
         LOGGER.debug("download success!  " + localTmpFile.getAbsolutePath());
-
         return null;
     }
 
