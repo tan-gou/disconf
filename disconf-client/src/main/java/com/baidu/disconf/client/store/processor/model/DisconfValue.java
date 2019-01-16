4e3f165d1e36167acac5301e9bb7ccd1011f6d21
haibing.cao <haibing.cao>
2019-01-16 09:58:30 +0800

    code optimize

--- disconf-client/src/main/java/com/baidu/disconf/client/store/processor/model/DisconfValue.java
+++ disconf-client/src/main/java/com/baidu/disconf/client/store/processor/model/DisconfValue.java
@@ -1,14 +1,11 @@
 package com.baidu.disconf.client.store.processor.model;
 
-import java.util.HashMap;
+import com.google.common.collect.Maps;
+
 import java.util.Map;
 
 /**
- * 配置的值，配置文件是properties, 配置项是string<br/>
- * 这个类是为了做兼容
- *
- * @author liaoqiqi
- * @version 2014-8-4
+ * 配置的值，配置文件是properties, 配置项是string，这个类是为了做兼容
  */
 public class DisconfValue {
 
@@ -16,7 +13,13 @@ public class DisconfValue {
     private String value;
 
     // 配置文件使用
-    private Map<String, Object> fileData = new HashMap<String, Object>();
+    private Map<String, Object> fileData = Maps.newHashMap();
+
+    public DisconfValue(String value, Map<String, Object> fileData) {
+        super();
+        this.value = value;
+        this.fileData = fileData;
+    }
 
     public String getValue() {
         return value;
@@ -26,11 +29,6 @@ public class DisconfValue {
         this.value = value;
     }
 
-    public DisconfValue(String value, Map<String, Object> fileData) {
-        super();
-        this.value = value;
-        this.fileData = fileData;
-    }
 
     public Map<String, Object> getFileData() {
         return fileData;
@@ -39,5 +37,4 @@ public class DisconfValue {
     public void setFileData(Map<String, Object> fileData) {
         this.fileData = fileData;
     }
-
 }
