354891aafcf96c644adcf07f1023ec55e0b23a3c
haibing.cao <haibing.cao>
2019-01-17 10:34:41 +0800

    code optimize

--- disconf-client/src/main/java/com/baidu/disconf/client/core/filetype/impl/DisconfAnyFileProcessorImpl.java
+++ disconf-client/src/main/java/com/baidu/disconf/client/core/filetype/impl/DisconfAnyFileProcessorImpl.java
@@ -5,14 +5,12 @@ import java.util.Map;
 import com.baidu.disconf.client.core.filetype.DisconfFileTypeProcessor;
 
 /**
- * Created by knightliao on 15/1/21.
- * <p/>
  * 任意文件的处理
  */
 public class DisconfAnyFileProcessorImpl implements DisconfFileTypeProcessor {
 
     @Override
-    public Map<String, Object> getKvMap(String fileName) throws Exception {
+    public Map<String, Object> getKvMap(String fileName) {
         // TODO Auto-generated method stub
         return null;
     }
