66ef411e3bbb9b0c988c0699506d584cea36fffc
haibing.cao <haibing.cao>
2019-01-17 16:25:09 +0800

    code optimize

--- disconf-client/src/main/java/com/baidu/disconf/client/core/processor/impl/DisconfCoreProcessUtils.java
+++ disconf-client/src/main/java/com/baidu/disconf/client/core/processor/impl/DisconfCoreProcessUtils.java
@@ -8,10 +8,7 @@ import org.slf4j.LoggerFactory;
 import com.baidu.disconf.client.common.update.IDisconfUpdate;
 import com.baidu.disconf.client.store.DisconfStoreProcessor;
 
-/**
- * @author liaoqiqi
- * @version 2014-8-4
- */
+
 public class DisconfCoreProcessUtils {
 
     protected static final Logger LOGGER = LoggerFactory.getLogger(DisconfCoreProcessUtils.class);
@@ -19,29 +16,17 @@ public class DisconfCoreProcessUtils {
     /**
      * 调用此配置影响的回调函数
      */
-    public static void callOneConf(DisconfStoreProcessor disconfStoreProcessor,
-                                   String key) throws Exception {
+    public static void callOneConf(DisconfStoreProcessor disconfStoreProcessor, String key) {
 
-        List<IDisconfUpdate> iDisconfUpdates = disconfStoreProcessor.getUpdateCallbackList(key);
-
-        //
         // 获取回调函数列表
-        //
-
+        List<IDisconfUpdate> iDisconfUpdates = disconfStoreProcessor.getUpdateCallbackList(key);
         // CALL
         for (IDisconfUpdate iDisconfUpdate : iDisconfUpdates) {
-
             if (iDisconfUpdate != null) {
-
                 LOGGER.info("start to call " + iDisconfUpdate.getClass());
-
-                // set defined
                 try {
-
                     iDisconfUpdate.reload();
-
                 } catch (Exception e) {
-
                     LOGGER.error(e.toString(), e);
                 }
             }
